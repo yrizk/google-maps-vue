544506c31a055a70a48f205deb66ca95c3c5b177
yrizk <yohan.rizk@gmail.com>
2017-09-30 17:33:17 -0400

    redone using the more sophisiticated webpack template. apologies for the double source

---
+++ google-maps/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="google-map">
+    <img src="./assets/logo.png">
+    <GoogleMap></GoogleMap>
+  </div>
+</template>
+
+<script>
+import GoogleMap from './components/GoogleMap'
+
+export default {
+  name: 'app',
+  components: {
+    GoogleMap
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-new Vue({
-  el: '#google-map',
-  render: h => h(App)
-})
